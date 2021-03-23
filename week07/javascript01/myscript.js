daeb36c7582a563f8f2be95f259709e4c1e4feb6
arielchuri <arielchuri@sparklelabs.com>
2021-03-22 21:32:44 -0400

    class work.

--- week07/javascript01/myscript.js
+++ week07/javascript01/myscript.js
@@ -1,1 +1,6 @@
 console.log("Hello world.")
+document.getElementById("myButton").innerHTML = "Yahoo!";
+document.getElementById("myButton").onclick = function() {
+  document.getElementById("myButton").innerHTML = "DO NOT PRESS!";
+  document.getElementById('myButton').style.color = "#00ff00";
+}
--- week07/javascript02/myscript.js
+++ week07/javascript02/myscript.js
@@ -2,9 +2,18 @@ console.log("Hello world.")
 document.getElementById("myButton").onclick = function() {
   console.log("yes");
   document.getElementById("word1").innerHTML = document.getElementById("field1").value;
+  document.getElementById("word1").innerHTML = "another word";
   document.getElementById("word2").innerHTML = document.getElementById("field2").value;
   document.getElementById("word3").innerHTML = document.getElementById("field3").value;
   document.getElementById("story").style.display = "block";
   document.getElementById("questions").style.display = "none";
   document.getElementById("myButton").style.display = "none";
+
+//  make word1 = field1 times field2;
+  document.getElementById('word1').innerHTML = document.getElementById("field1").value * document.getElementById("field2").value;
+  if (document.getElementById('field3').value == "+") {
+    document.getElementById('word1').innerHTML = document.getElementById("field1").valueAsNumber + document.getElementById("field2").valueAsNumber;
+  } else if (document.getElementById('field3').value == "-")  {
+    document.getElementById('word1').innerHTML = document.getElementById("field1").valueAsNumber - document.getElementById("field2").valueAsNumber;
+  }
 }
