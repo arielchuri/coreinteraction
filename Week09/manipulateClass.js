26d00391ec6e80dfd3fd9ccfc37a7b569a9b5ef4
arielchuri <arielchuri@sparklelabs.com>
2021-04-05 20:41:34 -0400

    wook 9 work

---
+++ Week09/manipulateClass.js
@@ -0,0 +1,7 @@
+document.getElementById("MyElement").classList.add('MyClass');
+
+document.getElementById("MyElement").classList.remove('MyClass');
+
+if ( document.getElementById("MyElement").classList.contains('MyClass') ){
+  document.getElementById("MyElement").classList.toggle('MyClass');
+}
