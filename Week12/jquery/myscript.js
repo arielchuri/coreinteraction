ea66306f29a49e6935817f2dffab126f063c6158
arielchuri <arielchuri@sparklelabs.com>
2021-05-03 15:45:50 -0400

    In class work.

--- Week12/jquery/myscript.js
+++ Week12/jquery/myscript.js
@@ -1,23 +1,20 @@
-$(document).ready(function(){
-// hides pi
+$(document).ready(function() {
+  // hides pi
   $('.p1').hide();
-// toggles p1 visibility
-  $('.p2').click(function(){
+  // toggles p1 visibility
+  $('.p2').click(function() {
     $('.p1').toggle();
   });
-// click on p3 to add a css class and run a function defined below.
-  $('.p3').click(function(){
+  // click on p3 to add a css class and run a function defined below.
+  $('.p3').click(function() {
     $('.p3').toggleClass('important');
     myFunctionName();
   });
-//menu toggles
-$(".menu-toggle").click(function() {
-  $(".hamburger").hide();
-  $("#menu").addClass("showmenu");
-
-})
-
-
+  //menu toggles
+  $(".menu-toggle").click(function() {
+    $(".hamburger").hide();
+    $("#menu").addClass("showmenu");
+  })
 });
 // when this functions run it checks to see if p3 has the 'important' class and shows an alert.
 // why does it never show 'False'?
