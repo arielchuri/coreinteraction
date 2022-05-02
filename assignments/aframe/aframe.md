417b9a1696dbfa63e6cf0f75634c060f2cedd816
arielchuri <github@sparklelabs.com>
2022-05-02 17:23:58 -0400

    lightbox and aframe

---
+++ assignments/aframe/aframe.md
@@ -0,0 +1,3 @@
+(aframe)[https://aframe.io/]
+(free 360 photos)[https://pixexid.com/search/360%20panoramic]
+(Local web server)[https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server]
--- assignments/javascript2/myscript.js
+++ assignments/javascript2/myscript.js
@@ -1,17 +1,17 @@
-var cars = ["Fred", "Mary","Joe"];
-console.log(cars.length);
-for ( var i = 0; i < cars.length ; i++ ) {
-//	console.log(i);
-	console.log(cars[i]);
-  console.log(i);
-  // Below I ouput the data to the webpage. I take the innerHTML of "jOutput" and add a space and then the value of cars[i] with every iteration of the loop.
-  document.getElementById("jOutput").innerHTML = document.getElementById("jOutput").innerHTML + " " + cars[i];
-}
-for ( var n = 0; n <= 20; n = n + 1 ) {
-	if ((n+1)%2==0){
-		console.log(n + " is odd.");
+// var cars = ["Fred", "Mary","Joe"];
+// console.log(cars.length);
+// for ( var i = 0; i < cars.length ; i++ ) {
+// //	console.log(i);
+// 	console.log(cars[i]);
+//   console.log(i);
+//   // Below I ouput the data to the webpage. I take the innerHTML of "jOutput" and add a space and then the value of cars[i] with every iteration of the loop.
+//   document.getElementById("jOutput").innerHTML = document.getElementById("jOutput").innerHTML + " " + cars[i];
+// }
+for ( var n = 1; n <= 20; n = n + 1 ) {
+	if (n%2==0){
+    document.getElementById("evenodd").innerHTML = document.getElementById("evenodd").innerHTML + n + " is even.</br>"
 	} else {
-		console.log(n);
+    document.getElementById("evenodd").innerHTML = document.getElementById("evenodd").innerHTML + n + " is odd.</br>"
  }
 };
 
@@ -21,6 +21,23 @@ var numset3 = [];
 console.log( numset1[0] + numset2[0] );
 for ( y =0; y < numset1.length; y++) {
   console.log( numset1[y] + numset2[y] );
+  document.getElementById("addingup").innerHTML = document.getElementById("addingup").innerHTML + (numset1[y]+numset2[y]) + ", ";
   numset3[y] = numset1[y] + numset2[y];
 }
 console.log(numset3);
+
+let str1 = "javascript";
+var str2 = "";
+console.log(str1);
+console.log(str1.length);
+for (p = 0; p < str1.length; p++){
+	if (p%2==0){
+    str2 += "Z";
+  } else {
+    str2 += str1[p];
+  }
+}
+  console.log(str2);
+// Example output:
+// jZvZsZrZpZ OR each letter on a new line
+// HINT: You can use  if((i+1) % 2 == 0) to check for even
