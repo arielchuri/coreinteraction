034f8ad6d75065d0dbdfea34bc5edc9fe1879b2d
arielchuri <github@sparklelabs.com>
2022-05-09 19:25:18 -0400

    inclass

--- assignments/p5js/sketch.js
+++ assignments/p5js/sketch.js
@@ -1,8 +1,15 @@
 function setup() {
   createCanvas(windowWidth-16, windowHeight-16);
-
+  background(220);
 }
 
 function draw() {
-  background(220);
+  ellipse(10,10,10,10);
+  ellipse(mouseX,mouseY,10,10);
+  ellipse(width/2,height/2,10,10);
+  for ( let y = 20 ; y < height ; y+=30 ) {
+    for ( let i = 20 ; i < width ; i+=30 ) {
+      ellipse( i, y, 20, 20);
+    }
+  }
 }
