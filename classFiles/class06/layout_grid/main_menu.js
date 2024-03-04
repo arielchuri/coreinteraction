c7f071e9a220d7a915cba147c76b7af6a9880754
Ariel Churi <github@sparklelabs.com>
2024-03-04 18:34:56 -0500

    mainnav

--- classFiles/class06/layout_grid/main_menu.js
+++ classFiles/class06/layout_grid/main_menu.js
@@ -1,19 +1,16 @@
 console.log("main menu");
 
 const header= `
-<header>
+<header class="topnav" id="myTopnav">
     <nav>
         <ul>
             <li><a href="index.html" class="home">home</a></li>
             <li><a href="grid.html" class="projects">projects</a></li>
             <li><a href="contact.html" class="contact">contact</a></li>
         </ul>
-    <nav>
+    </nav>
 </header>
-<label for="checkbox">
-<!-- <i class="fa fa-bars menu-icon"></i> -->
-<i class="fas fa-caret-down menu-icon"></i>
-</label>
+</div>
 `
 
 document.querySelector("body").insertAdjacentHTML("afterbegin", header);
