4ed766d0c52b5a47c0281786be6d82ca8af16318
Ariel Churi <github@sparklelabs.com>
2024-03-18 18:24:15 -0400

    class

---
+++ classFiles/class08/blank_webpage/main_menu.js
@@ -0,0 +1,31 @@
+console.log("main menu");
+
+const header= `
+<header class="header">
+<div class="headercontainer">
+    <!-- Logo -->
+    <a href="#" class="logo">AC</a>
+    <!-- Hamburger icon -->
+    <input class="side-menu" type="checkbox" id="side-menu"/>
+    <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+    <!-- Menu -->
+    <nav class="nav">
+        <ul class="menu">
+            <li><a href="#">Home</a> </li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
+`
+const footer= `
+<footer>
+    <div class="footercontainer">
+        <p>footer</p>
+    </div>
+</footer>
+`
+
+document.querySelector("body").insertAdjacentHTML("afterbegin", header);
+document.querySelector("body").insertAdjacentHTML("beforeend", footer);
