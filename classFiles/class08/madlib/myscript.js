344a3b25737a40fe6d0531f1a26b561bd7cdacb7
Ariel Churi <github@sparklelabs.com>
2024-03-25 18:31:19 -0400

    madlib

--- classFiles/class08/madlib/myscript.js
+++ classFiles/class08/madlib/myscript.js
@@ -1,6 +1,11 @@
 function displayText() {
-    const userInput = document.getElementById('userInput').value;
-    document.getElementById('output').innerText = userInput;
+    const walkingInput = document.getElementById('walkingInput').value;
+    const dogInput = document.getElementById('dogInput').value;
+    const catInput = document.getElementById('catInput').value;
+    document.getElementById('walking').innerText = walkingInput;
+    document.getElementById('dog').innerText = dogInput;
+    document.getElementById('cat').innerText = catInput;
+    document.getElementById('story').style.visibility = 'visible';
 }
 
 const form = document.getElementById('myForm');
